14b6b20b127fc6ded05994db4d4efd0afb972d6a
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-07-04 14:58:24 +0300

    done

---
+++ index.js/task-04.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const counterValue = {
+    value: 0,
+
+    decrement () {
+        // console.log(this); //'decrement -> this'
+        this.value -= 1;
+    },
+
+    increment () {
+        // console.log(this); //'increment -> this',
+        this.value += 1;
+    }
+}
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const valueEl = document.querySelector('#value');
+
+
+
+
+decrementBtn.addEventListener('click', function () {
+
+    counterValue.decrement();
+    valueEl.textContent = counterValue.value;
+});
+
+
+incrementBtn.addEventListener('click', function () {
+
+    counterValue.increment();
+    valueEl.textContent = counterValue.value;
+});
