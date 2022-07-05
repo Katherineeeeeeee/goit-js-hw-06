56df9c7b2a5b9bea90b2972c8fb691b0bfc89127
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-07-05 21:27:35 +0300

    done

--- index.js/task-09.js
+++ index.js/task-09.js
@@ -1,3 +1,11 @@
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+});
+
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
