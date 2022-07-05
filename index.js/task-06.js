986062943bd3ce487dd34be1cd6ad0ff4a37a5d7
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-07-05 15:55:03 +0300

    done

--- index.js/task-06.js
+++ index.js/task-06.js
@@ -4,13 +4,12 @@ const inputEl = document.querySelector('#validation-input');
 
 inputEl.addEventListener('blur', () => {
     if(inputEl.value.length === Number(inputEl.dataset.length)) {
-        inputEl.classList.add('invalid');
-        inputEl.classList.remove('valid');
+        inputEl.classList.add('valid');
+        inputEl.classList.remove('invalid');
     } else {
-    inputEl.classList.remove('invalid');
-    inputEl.classList.add('valid');
+    inputEl.classList.remove('valid');
+    inputEl.classList.add('invalid');
     }
 
 });
 
-console.log(inputEl)
