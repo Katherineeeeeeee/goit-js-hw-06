b7ae5c86f14e4ad10c5324d8f79e479d7d102e61
Katherineeeeeeee <katherine.dudar.7@gmail.com>
2022-07-06 15:51:13 +0300

    not done

--- index.js/task-10.js
+++ index.js/task-10.js
@@ -1,3 +1,39 @@
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+
+
+createBtn.addEventListener('click', () => {});
+destroyBtn.addEventListener('click', () => {});
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+}
+
+
+//1. Напиши скрипт створення і очищення колекції елементів. 
+// Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. 
+// Натисненням на кнопку Очистити, колекція елементів очищається.
+
+
+//2. Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, 
+// скільки вказано в amount і додає їх у div#boxes.
+
+//3. Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+
+// Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
+// function getRandomHexColor() вже є
+
+
+
+
+//+ 4. Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+//?Якщо у властивість innerHTML записати порожній рядок, то вміст елемента буде очищено. Це простий і швидкий спосіб видалення всього вмісту.
